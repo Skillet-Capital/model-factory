b73278c90f543143c566168524f805ee77609d1c
Andrew Warner <warneracw@gmail.com>
2024-12-06 11:13:31 -0500

    update ignition (#10)

--- ignition/modules/KettleAssetFactoryInitialization.ts
+++ ignition/modules/KettleAssetFactoryInitialization.ts
@@ -12,6 +12,16 @@ const kettleAssetFactoryInitialization = buildModule("KettleAssetFactoryInitiali
 
   m.call(factory, "initialize", [account, implementation]);
 
+  m.call(factory, "setOperator", ["0x5aBe43c199E67cA55061fc90fd29d8F0B4403175", true]);
+
+  m.call(factory, "setOperator", ["0x962fb97954a5d9cc6801a724ce783ea639d90d2e", true], { id: "redemptionManager" });
+
+  m.call(factory, "setOperator", ["0xE2e91f1F1E2095e33d4DFf1E87FFB9Da21498098", true], { id: "kettle_v2" });
+
+  m.call(factory, "setOperator", ["0x26d573FdebAc88D4487F4635A3a573a934a0048a", true], { id: "kettle_v2_1" });
+
+  m.call(factory, "setOperator", ["0x14fc7A3A449468C35E8a9c9432F68c378E6485BE", true], { id: "kettle_v2_2" });
+
   return { factory, proxy, proxyAdmin };
 });
 
