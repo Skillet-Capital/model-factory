32681c5669ff497e1884f44f96cfd2643263d5ba
Andrew Warner <warneracw@gmail.com>
2025-01-13 11:31:11 -0500

    update deployments

--- ignition/modules/DEV/KettleAssetFactoryInitialization.ts
+++ ignition/modules/DEV/KettleAssetFactoryInitialization.ts
@@ -12,7 +12,8 @@ const kettleAssetFactoryInitialization = buildModule("kettle_asset_factory_init_
 
   m.call(factory, "initialize", [account, implementation]);
 
-  m.call(factory, "setOperator", ["0x34C6507f5862b1fcdf2Ce837583969A93740b937", true]);
+  m.call(factory, "setOperator", ["0x34C6507f5862b1fcdf2Ce837583969A93740b937", true]); // MARKETPLACE
+  m.call(factory, "setOperator", ["0x89b3f2b574d85716661221E4D2396388CaFDf630", true], { id: "bridge_operator"}); // BRIDGE
 
   return { factory, proxy, proxyAdmin };
 });
--- ignition/modules/PROD/KettleAssetFactory.ts
+++ ignition/modules/PROD/KettleAssetFactory.ts
@@ -13,6 +13,15 @@ const kettleAssetFactoryInitialization = buildModule("kettle_asset_factory_init_
   m.call(factory, "initialize", [account, implementation]);
 
   m.call(factory, "setOperator", ["0x0d5212403Be4dA94765e7b75F5842ff2290e34E1", true]);
+  m.call(factory, "setOperator", ["0xc5d387CAD90F2B962D562ab4DFB2b9F8805355ee", true], { id: "bridge_operator"});
+
+  m.call(factory, "setApprovedTransfer", [
+    "0x14c7fd5669d2c441fabbbd16624838efe5c2b26c",
+    "0x7e4a82326dcb5f40851dcf67b145a3ee68fb1d19",
+    "0x6b83081D6Dd1b817B8c44535578C4C7bA765D675",
+    "100001",
+    true
+  ], { id: "approved_transfer_1" });
 
   return { factory, proxy, proxyAdmin };
 });
